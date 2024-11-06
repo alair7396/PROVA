0fd7f8c20708b25f2a7f51e279807a37a807fb6d
alair7396 <alair_luiz@estudante.sc.senai.br>
2024-11-05 21:51:02 -0400

    icone deletar

--- src/paginas/PaginaListaProdutos/PaginaListaProdutos.jsx
+++ src/paginas/PaginaListaProdutos/PaginaListaProdutos.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import BotaoCustomizado from "../../comum/componentes/BotaoCustomizado/BotaoCustomizado";
 import Principal from "../../comum/componentes/Principal/Principal";
 import './PaginaListaProdutos.css'
+import { FaTrash } from "react-icons/fa";
 const PaginaListaProdutos=()=>{
     const[nome,setNome]=useState('')
     const[preco,setPreco]=useState('')
@@ -26,27 +27,30 @@ const PaginaListaProdutos=()=>{
          
     }
     const remover=(index)=>{
-        if (confirm(`Tem certeza que deseja excluir? ${nome}`
+        const produto = lista[index];
+        if (confirm(`Tem certeza que deseja excluir? ${produto.nome}?`
     )){const atualizar=([...lista])
-        atualizar.splice(index.target.value,1)
+        atualizar.splice(index,1)
         setLista([...atualizar])}
         
         
     }
+    
     return(
         <>
         <Principal titulo={`Lista de Produtos (${lista.length})`}
       voltarPara="/">
             <div className="padrao">
-                <input type="text" placeholder="nome" value={nome} onChange={input}/>
-                <input type="text" placeholder="preco" value={preco} onChange={input2}/>
+                <input type="text" placeholder="Nome" value={nome} onChange={input}/>
+                <input type="text" placeholder="Preço" value={preco} onChange={input2}/>
                 <BotaoCustomizado aoClicar={botao}>enviar</BotaoCustomizado>
             </div>
                {lista.map((produto,index)=>(
                 <div  className="padrao2" key={index}>
-                    <strong>Nome:</strong>{produto.nome} <br/>
+                    <strong>Nome:</strong>{produto.nome} 
+                    <br/>
                     <strong>Preço:</strong>{produto.preco}
-                    <div className="padrao3"><button value={index} className="padrao4" onClick={remover}> R </button></div>
+                    <div className="padrao3"><button  className="padrao4" onClick={()=> remover(index)} >  <FaTrash /></button></div>
                 </div>
                 
             ))}
